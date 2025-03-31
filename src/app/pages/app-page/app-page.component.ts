4687b92d3078dcc844bc94dfcbd6459c51b21464
Alexandr <alexzavgorodnii@users.noreply.github.com>
2025-03-31 17:21:39 +0200

    enhance app-page layout with sidebar navigation; integrate Angular Material components for improved UI

--- src/app/pages/app-page/app-page.component.ts
+++ src/app/pages/app-page/app-page.component.ts
@@ -2,17 +2,39 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { LucideAngularModule, Package } from 'lucide-angular';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-app-page',
-  imports: [RouterOutlet, MatSidenavModule, LucideAngularModule],
+  imports: [RouterOutlet, MatSidenavModule, LucideAngularModule, MatButtonModule, MatDividerModule, MatIconModule],
   template: `
     <mat-drawer-container class="h-screen w-full">
-      <mat-drawer mode="side" opened>
-        <a class="block text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300" [href]="'/'">
-          <span class="sr-only">Home</span>
-          <i-lucide [img]="Package" class="h-8 w-8"></i-lucide>
-        </a>
+      <mat-drawer mode="side" opened class="sidebar">
+        <div class="flex flex-col p-3">
+          <a class="flex flex-row items-center gap-2" [href]="'/'">
+            <i-lucide [img]="Package" class="h-8 w-8"></i-lucide>
+            <span>Translatoo</span>
+          </a>
+        </div>
+        <mat-divider></mat-divider>
+        <nav class="flex flex-col p-3">
+          <ul>
+            <li>
+              <a mat-button href="https://www.google.com/" target="_blank" class="w-full !justify-start">
+                <mat-icon>dashboard</mat-icon>
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <a mat-button href="https://www.google.com/" target="_blank" class="w-full !justify-start">
+                <mat-icon>folder_copy</mat-icon>
+                Projects
+              </a>
+            </li>
+          </ul>
+        </nav>
       </mat-drawer>
       <mat-drawer-content><router-outlet /></mat-drawer-content>
     </mat-drawer-container>
